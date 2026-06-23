4bab42e3d64c37e39f514ceb0045a40a3aca846d
Eddie <emurr93@gmail.com>
2026-06-23 15:06:19 -0600

    updated

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        int number = -1;
+        List<int> numbers = new List<int>();
+        do
+        {
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        Console.Write("Enter number: ");
+        number = int.Parse(Console.ReadLine());
+        numbers.Add(number);
+        } while (number != 0);
+        int sum = 0;
+        foreach (int num in numbers) 
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}.");
+
     }
 }
