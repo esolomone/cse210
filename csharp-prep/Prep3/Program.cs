bf04403e94e061540e9af28bfc058d7ed1a9fde9
Eddie <emurr93@gmail.com>
2026-06-23 15:06:06 -0600

    updated

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 11);
+        int magicNumber = randomGenerator.Next(1, 100);
         int guessNumber = -1;
         do
         {
